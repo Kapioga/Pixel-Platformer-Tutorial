9f85294556299a057a72eca7bbcd7250a1ce1b26
Kapioga <ldaleyvo@gmail.com>
2022-08-08 17:47:57 -0400

    player update

--- Player.gd
+++ Player.gd
@@ -9,10 +9,16 @@ func _ready():
 #Every physics frame in game
 func _physics_process(delta):
 	velocity.y +=2
+	
 	#Buttons
-	if Input.is_action_just_pressed("ui_right"):
-		velocity.x=10
-	elif Input.is_action_just_pressed("ui_left"):
-		velocity.x=-10
+	if Input.is_action_pressed("ui_right"):
+		velocity.x=100
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x=-100
+	else:
+		velocity.x=0
+		
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y=-100
 		
 	velocity = move_and_slide(velocity) #Moves character and detects collisions/Gravity
