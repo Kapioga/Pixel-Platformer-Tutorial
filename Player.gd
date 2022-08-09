b427c9ff004e985da1af14ef11b40c7aa65efd8e
Kapioga <ldaleyvo@gmail.com>
2022-08-09 12:11:33 -0400

    Updated movement

--- Player.gd
+++ Player.gd
@@ -8,17 +8,30 @@ func _ready():
 
 #Every physics frame in game
 func _physics_process(delta):
-	velocity.y +=2
+	apply_gravity()
 	
-	#Buttons
-	if Input.is_action_pressed("ui_right"):
-		velocity.x=100
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x=-100
+	#Adds friction
+	var input = Vector2.ZERO
+	input.x= Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x==0:
+		apply_friction()
 	else:
-		velocity.x=0
+		apply_acceleration(input.x)
 		
 	if Input.is_action_just_pressed("ui_up"):
-		velocity.y=-100
-		
+		velocity.y=-120
+
 	velocity = move_and_slide(velocity) #Moves character and detects collisions/Gravity
+
+#Gravity
+func apply_gravity():
+	velocity.y+=4
+
+#Friction
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, 20)
+	
+#Movement
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, 50 * amount, 20)
